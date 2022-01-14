fbdf7a759370ab05e18358956d91a681011ecf2e
eddie nyambudzi <e.nyambudzi@googlemail.com>
2022-01-14 23:57:40 +0000

    testing open ssl install

---
+++ scripts/04_install_openssl.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sudo -i
+
+CURRENT_DIR=$(pwd)
+sudo yum update -y
+#sudo yum group install 'Development Tools' -y
+sudo yum install  -y bison byacc cscope ctags cvs diffstat doxygen flex gcc gcc-c++ gcc-gfortran gettext git indent intltool libtool patch patchutils rcs redhat-rpm-config rpm-build subversion swig systemtap
+sudo yum install perl-core zlib-devel -y
+
+cd /usr/local/src/
+sudo wget https://www.openssl.org/source/openssl-1.1.1c.tar.gz
+sudo tar -xf openssl-1.1.1c.tar.gz
+cd openssl-1.1.1c
+sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
+sudo make
+sudo make test
+sudo make install
+
+cd /etc/ld.so.conf.d/
+cp /tmp/assets/openssl/openssl-1.1.1c.conf /etc/ld.so.conf.d/openssl-1.1.1c.conf
+sudo ldconfig -v
+sudo mv /bin/openssl /bin/openssl.backup
+
+cp /tmp/assets/openssl/openssl.sh /etc/profile.d/openssl.sh
+source /etc/profile.d/openssl.sh
+openssl version -a
+
+cd $CURRENT_DIR
