44dc29e1010148e14f519ac57234ce9e7604b8db
eddie nyambudzi <e.nyambudzi@googlemail.com>
2022-01-18 16:57:53 +0000

    tidy up

--- scripts/30_install_awslogs_amazonlinux2.sh
+++ scripts/30_install_awslogs_amazonlinux2.sh
@@ -2,7 +2,4 @@
 
 sudo -i
 
-#sudo yum install -y amazon-cloudwatch-agent
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
-sudo rpm -U ./amazon-cloudwatch-agent.rpm
-rm -rf amazon-cloudwatch-agent.rpm
+sudo yum install -y amazon-cloudwatch-agent
